c3d32e1c3b8f83c6d822263a3cb701cb10f14a19
Allan Hipolito <allanhipolito66@gmail.com>
2025-11-23 00:29:50 -0300

    feat: add oasis infra with TEE, create unit tests to grant

--- packages/aztec/contracts/src/utils/sponsored_fpc.d.ts
+++
@@ -1,7 +0,0 @@
-import { type ContractInstanceWithAddress } from '@aztec/aztec.js/contracts';
-import type { Wallet } from '@aztec/aztec.js/wallet';
-import type { LogFn } from '@aztec/foundation/log';
-export declare function getSponsoredFPCInstance(): Promise<ContractInstanceWithAddress>;
-export declare function getSponsoredFPCAddress(): Promise<import("@aztec/stdlib/aztec-address").AztecAddress>;
-export declare function setupSponsoredFPC(deployer: Wallet, log: LogFn): Promise<void>;
-//# sourceMappingURL=sponsored_fpc.d.ts.map
